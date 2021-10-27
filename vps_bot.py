7b231c721e5d332881270a34316fdc3e1d28d466
Shehab Gad <shehabgad2@gmail.com>
2021-10-27 16:59:12 +0200

    sd

--- vps_bot.py
+++ vps_bot.py
@@ -5,7 +5,7 @@ import selenium
 
 host = socket.gethostname()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect_ex((host, 28000))
+s.connect_ex(("172.26.9.86", 28000))
 size = 10485760
 
 
